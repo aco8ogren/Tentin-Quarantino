333ece28d381862050a46640218044296f4dae9e
Josh <jlassman@caltech.edu>
2020-05-19 14:21:58 -0700

    Fixed Plotting in county plotter

--- Josh/CountyClustering/ClusterByDeaths.py
+++ Josh/CountyClustering/ClusterByDeaths.py
@@ -8,7 +8,7 @@ cwd=repo.working_dir
 os.chdir(cwd)
 
 
-def JoshMeansClustering(fipsList,date,CritCases=50,Fpath=None):
+def JoshMeansClustering(fipsList,date,d_thres=50,Fpath=None):
     covidDF=pd.read_csv('data/us/covid/nyt_us_counties.csv')
     covidDF['date']=pd.to_datetime(covidDF['date'])
     if date is not None:
@@ -16,27 +16,37 @@ def JoshMeansClustering(fipsList,date,CritCases=50,Fpath=None):
     else:
         date=covidDF.date.max()
     
+    # Get df that relates fips to lat/long
     geoDF=pd.read_csv(r'Josh\Processing\Processed Data\GeoData.csv')
+    # Add missing Ogala entry
     Ogala={k:v for k,v in zip(['fips','long','lat'],[46102,-102.821318,43.352275])}
     geoDF=geoDF.append(Ogala,ignore_index=True)
+    # Add lat/long to main df
     df=pd.merge(covidDF[['fips','deaths','date']],geoDF,how='left',on='fips')
     
-
+    # take only df rows for corresponding to the cluster date (=train_til in main code)
     df=df[df.date==date].drop(columns='date')
+    # take only rows for fips in the fips to cluster list
     df=df[df.fips.isin(fipsList)]
     
+    # Seperate Alaska and Hawaii, as it does not make sense to cluster them geographically with continental counties
+        # give them their own seperate clusters as they are usually below D_THRES
     AL_HA=df[df.long<-128]
     df=df[df.long>=-128]
     AL_HA['cluster']=np.nan*np.ones(len(AL_HA))
     AL_HA.loc[AL_HA.lat>40,'cluster']=-1
     AL_HA.loc[AL_HA.lat<=40,'cluster']=-2
 
+    # Sort by long than lat and take the cumulative deaths along longitude
     df=df.sort_values(by=['long','lat']).reset_index(drop=True)
     df['LongDeaths']=df.deaths.cumsum()
 
-
-    TargetNumClusters=df.deaths.sum()/CritCases
+    # estimate number of clusters
+    TargetNumClusters=df.deaths.sum()/d_thres
+    # estimate number of columnar clusters
     LongRes=((TargetNumClusters)**.5)
+    # threshold for deaths per columnar cluster
+        # i.e. the number of deaths each columnar cluster is guaranteed to match or exceed
     critLong=np.floor(df.deaths.sum()/LongRes)
     df['LongQDeaths']=df['LongDeaths']
     df['LongQ']=np.nan*np.ones(len(df))
@@ -61,8 +71,8 @@ def JoshMeansClustering(fipsList,date,CritCases=50,Fpath=None):
         DF.loc[:,'cluster']=np.nan*np.ones(len(DF))
         boundaries=[0]
        
-        while DF.iloc[-1].LatQDeaths>CritCases:
-            boundaries.append((DF.LatQDeaths.values>=CritCases).argmax()+1)
+        while DF.iloc[-1].LatQDeaths>d_thres:
+            boundaries.append((DF.LatQDeaths.values>=d_thres).argmax()+1)
             DF.iloc[boundaries[-2]:boundaries[-1],DF.columns=='cluster']=int(Q)
             DF.LatQDeaths-=DF.iloc[boundaries[-1]-1].LatQDeaths
             # print(DF)
